ef6a8fb267b16e0688a34a7a7e5a2ab5164d0782
Raymond <shafreeck@gmail.com>
2024-03-05 12:58:41 +0800

    Update config and README

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,10 +83,10 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: '文档',
             items: [
               {
-                label: 'Tutorial',
+                label: '课程',
                 to: '/docs/Flutter 开发/课程大纲',
               },
             ],
@@ -95,16 +95,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: '四喵',
+                href: 'https://smilemiao.com/',
               },
             ],
           },
@@ -113,12 +105,12 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/smile-learning',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Smile Learning, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
